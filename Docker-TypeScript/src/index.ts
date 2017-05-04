d2a45d5755db578cd943316d46b273171a105b1a
Andre Weinand <aweinand@microsoft.com>
2017-05-05 00:02:44 +0200

    fix indentation

--- Docker-TypeScript/src/index.ts
+++ Docker-TypeScript/src/index.ts
@@ -4,12 +4,12 @@ let reqCnt = 1;
 
 http.createServer((req, res) => {
 
-	const message = `Request Count: ${reqCnt}`;
+  const message = `Request Count: ${reqCnt}`;
 
-	res.writeHead(200, { 'Content-Type': 'text/html' });
-	res.end(`<html><head><meta http-equiv="refresh" content="2"></head><body>${message}</body></html>`);
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(`<html><head><meta http-equiv="refresh" content="2"></head><body>${message}</body></html>`);
 
-	console.log("handled request: " + reqCnt++);
+  console.log("handled request: " + reqCnt++);
 }).listen(3000);
 
 console.log('server running on port 3000');
